b60d67ff3f4e146e20b374d806e8fd733ff232c3
sean <seanluse41@gmail.com>
2019-12-16 13:01:46 +0900

    added CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,15 +19,24 @@ class PostsController < ApplicationController
     end
 
     def edit
-    
+    @post = Post.find(params[:id])
     end
 
     def destroy
-    
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
     end
 
     def update
-    
+    @post = Post.find(params[:id])
+
+        if @post.update(post_params)
+            redirect_to @post
+        else
+            render 'edit'
+        end
     end
 
     def show
