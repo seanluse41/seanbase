9e69465a910e5d691773a1f683eed5e957a9f910
sean <seanluse41@gmail.com>
2020-02-14 12:30:15 +0900

    redirect non www to www domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   around_action :switch_locale
+  before_action :redirect_subdomain
 
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
+  end
+
+  def redirect_subdomain
+    if request.host == "http://seanbase.com"
+      redirect_to "www.seanbase.com" + request.fullpath, :status => 301
+    end
   end
 end
