07f962354c1df905a9778650b394aab9add3470b
sean <seanluse41@gmail.com>
2020-04-07 15:39:39 +0900

    upgrade puma etc. fix form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,6 +47,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :caption, :image)
+    params.require(:post).permit(:title, :content, :image)
   end
 end
