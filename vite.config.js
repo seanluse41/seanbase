22708d9c3910b297c61ec1a8a4765ed63af7ed73
seanluse41 <seanluse41@gmail.com>
2024-08-25 14:17:26 +0900

    it worked, so which setting was it?

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	build: {
-	  rollupOptions: {
-		external: ['fs']
-	  }
-	},
-	assetsInclude: ['**/*.ttf', '**/*.png'],
-  });
+	plugins: [sveltekit()]
+});
