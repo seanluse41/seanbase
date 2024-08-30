ed692f684737d7823000e5175da206dd34c5cdac
sean <seanluse41@gmail.com>
2024-08-30 11:43:06 +0900

    project cards

--- src/routes/kintone/+page.svelte
+++ src/routes/kintone/+page.svelte
@@ -1,8 +1,31 @@
 <script>
+    import ProjectCard from "../../components/projectCard.svelte";
+    import Loader from "../../components/loader.svelte";
+    import { fly } from "svelte/transition";
     export let data;
-    console.log(data)
+    let cardDelay = 100; // Delay between each card in milliseconds
 </script>
 
-{#each data.projects as project}
-    <p>{project.title.value}</p>
-{/each}
+{#await data}
+<Loader />
+{:then data}
+<div class="flex flex-row flex-wrap">
+    {#each data.projects as project, i}
+        <div
+            in:fly|global={{
+                y: 200,
+                duration: 2000,
+                delay: i * cardDelay,
+            }}
+        >
+            <ProjectCard
+                title={project.title.value}
+                description={project.description.value}
+                type={project.type.value}
+                link={project.link.value}
+                imageURL={project.imageURL}
+            />
+        </div>
+    {/each}
+</div>
+{/await}
