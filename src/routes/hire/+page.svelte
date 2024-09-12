681c9c12cda961409f7f0c7e9622e92cbfb3c25d
sean <seanluse41@gmail.com>
2024-09-13 00:27:48 +0900

    subtext

--- src/routes/hire/+page.svelte
+++ src/routes/hire/+page.svelte
@@ -1,15 +1,20 @@
 <script>
     import { _ } from "svelte-i18n";
-    import { Card, Heading, Img } from "flowbite-svelte";
+    import { Card, Heading, Img, P } from "flowbite-svelte";
     import ContactForm from "../../components/contactForm.svelte";
-    import workTogether from "../../lib/process-flow-diagram.svg"
+    import workTogether from "../../lib/process-flow-diagram.svg";
 </script>
 
 <Card size="xl" class="mt-8">
     <div class="flex flex-col lg:flex-row justify-center">
-        <Heading class="text-slate-700 self-center mt-8 lg:my-8 w-auto"
-            >{$_("hire.title")}</Heading
-        >
+        <div class="flex flex-col self-center">
+            <Heading class="text-slate-700 self-center mt-8 lg:my-8 w-auto"
+                >{$_("hire.title")}</Heading
+            >
+            <P
+                >{$_("hire.subtext")}</P
+            >
+        </div>
         <Img src={workTogether} size="max-w-xs" />
     </div>
     <ContactForm isModal={false} />
